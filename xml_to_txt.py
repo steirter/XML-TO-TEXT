4f05d453a4a79faa181639b92db519f5031b71b9
ruben <ruben.vansteirteghem@student.jdg.be>
2023-11-03 15:08:55 +0100

    clean up

--- xml_to_txt.py
+++ xml_to_txt.py
@@ -1,6 +1,6 @@
 from bs4 import BeautifulSoup as bs
 
-with open('/source.xml', 'r', encoding="utf-8") as f:
+with open('source/source.xml', 'r', encoding="utf-8") as f:
     data = f.read()
 
 bs_data = bs(data, "xml")
@@ -8,7 +8,7 @@ b_item = bs_data.find_all('content:encoded')
 i = 0
 
 for item in b_item:
-    f = open("./textfiles/pagina_%s.txt" % str(i), "w+", encoding="utf-8")
+    f = open("output/pagina_%s.txt" % str(i), "w+", encoding="utf-8")
     for img_tag in item.find_all("img width="):
         img_tag.extract()
     cleaned_text = item.getText()
